ebfd32dae447c0ab31c4ab8eae1afc1ceb790c7f
Shawn Inder <shawninder@gmail.com>
2020-05-17 17:46:38 -0400

    Add image to test deployment

--- components/teaser.js
+++ components/teaser.js
@@ -1,6 +1,7 @@
 export default function Teaser () {
   return (
     <div id='teaser'>
+      <img src='/Rouge-canyon-rapids-from-above.jpg' />
       <style jsx>
         {`
           #teaser {
@@ -8,6 +9,9 @@ export default function Teaser () {
             width: 600px;
             height: 320px;
             background-color: black;
+          }
+          img {
+            width: 100%;
           }
         `}
       </style>
